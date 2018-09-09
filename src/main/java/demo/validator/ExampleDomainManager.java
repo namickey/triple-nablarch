2668f1da35edf492b5e5258436fe1b9f4c1049e3
namickey <teethdai@gmail.com>
2018-09-10 01:34:57 +0900

    add.

---
+++ src/main/java/demo/validator/ExampleDomainManager.java
@@ -0,0 +1,18 @@
+package demo.validator;
+
+import nablarch.core.validation.ee.DomainManager;
+
+/**
+ * {@link DomainManager} の実装クラス。
+ * <p/>
+ * ドメインを定義したBeanクラスを返却する。
+ *
+ * @author Nabu Rakutaro
+ */
+public class ExampleDomainManager implements DomainManager<ExampleDomainType> {
+    @Override
+    public Class<ExampleDomainType> getDomainBean() {
+        // ドメインBeanのClassオブジェクトを返す
+        return ExampleDomainType.class;
+    }
+}
